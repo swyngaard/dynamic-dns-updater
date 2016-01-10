2f021f78bf3f297036b0ada2b2a2890238f572c0
Sebastian Wyngaard <basbot@gmail.com>
2016-01-09 23:04:15 -0800

    Retrieving external IP address, checking if sub-domain IP address has changed and modifying the sub-domain IP address if a change has occured

---
+++ updater.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python
+
+import requests
+import argparse
+
+def create_cpanel(cpanel_user, cpanel_pass, cpanel_url, dns_domain, dns_sub):
+    cpanel = {
+        'url' : cpanel_url + '/json-api/cpanel',
+        'params' : {
+            'cpanel_jsonapi_user' : cpanel_user,
+            'cpanel_jsonapi_apiversion' : '2',
+            'cpanel_jsonapi_module' : 'ZoneEdit',
+            'domain' : dns_domain,
+            'name' : dns_sub + '.' + dns_domain + '.' #NB! note the dot at the end of full subdomain
+        }, 
+        'user' : cpanel_user, 
+        'pass' : cpanel_pass
+    }
+    
+    return cpanel
+
+#TODO Add SSL certification
+def get_subdomain_property(cpanel, property):
+    params = cpanel['params']
+    params['cpanel_jsonapi_func'] = 'fetchzone'
+    
+    response = requests.get(cpanel['url'], params, auth=(cpanel['user'], cpanel['pass']),  verify=False)
+    
+    result = response.json()['cpanelresult']['data'][0]['record']
+    
+    if result:
+            return result[0][property]
+    
+    return None
+
+def edit_subdomain_address(cpanel, dns_line, dns_address):
+    params = cpanel['params']
+    params['cpanel_jsonapi_func'] = 'edit_zone_record'
+    params['type'] = 'A'
+    params['Line'] = dns_line
+    params['address'] = dns_address
+    
+    requests.get(cpanel['url'], params, auth=(cpanel['user'], cpanel['pass']),  verify=False)
+
+#Use external service to get IP address
+def get_ip_address():
+    response = requests.get('http://ipinfo.io/ip')
+    
+    return response.text
+
+def main():
+    parser = argparse.ArgumentParser(description="Dynamic DNS updater")
+    parser.add_argument("-u",  "--username",  help="cPanel username")
+    parser.add_argument("-p",  "--password",  help="cPanel password")
+    parser.add_argument("domain",  help="Domain to be modified")
+    parser.add_argument("sub_domain", nargs='+', help="List of sub-domain names to be updated (excluding the qualified domain suffix). eg. hello.example.com would simply translate to hello")
+    parser.add_argument("url", help="cPanel URL e.g. https://example.com:2083")
+    
+    args = parser.parse_args()
+    
+    cpanel_user = args.username
+    cpanel_pass = args.password
+    cpanel_url = args.url
+    dns_domain = args.domain
+    dns_subs = args.sub_domain
+    
+    ip_address = get_ip_address()
+    
+    #Each subdomain shares the same address above
+    for dns_sub in dns_subs:
+        cpanel = create_cpanel(cpanel_user, cpanel_pass, cpanel_url, dns_domain, dns_sub)
+        dns_address = get_subdomain_property(cpanel, 'address')
+        
+        #Check if IP address has changed
+        if ip_address != dns_address:
+            dns_line = get_subdomain_property(cpanel, 'line')
+            edit_subdomain_address(cpanel, dns_line, ip_address)
+        
+
+if __name__ == '__main__':
+    main()
