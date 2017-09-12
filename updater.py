38a744304fc09e04870712fb2ba34afc05aa31ba
Sebastian Wyngaard <basbot@gmail.com>
2017-09-12 20:46:57 +0000

    Enable SSL verification

--- updater.py
+++ updater.py
@@ -1,37 +1,39 @@
-#! /usr/bin/env python
+#!/usr/bin/env python3
 
 import requests
 import argparse
 
+
 def create_cpanel(cpanel_user, cpanel_pass, cpanel_url, dns_domain, dns_sub):
     cpanel = {
-        'url' : cpanel_url + '/json-api/cpanel',
-        'params' : {
-            'cpanel_jsonapi_user' : cpanel_user,
-            'cpanel_jsonapi_apiversion' : '2',
-            'cpanel_jsonapi_module' : 'ZoneEdit',
-            'domain' : dns_domain,
-            'name' : dns_sub + '.' + dns_domain + '.' #NB! note the dot at the end of full subdomain
+        'url': cpanel_url + '/json-api/cpanel',
+        'params': {
+            'cpanel_jsonapi_user': cpanel_user,
+            'cpanel_jsonapi_apiversion': '2',
+            'cpanel_jsonapi_module': 'ZoneEdit',
+            'domain': dns_domain,
+            'name': dns_sub + '.' + dns_domain + '.'  # NB! note the dot at the end of full subdomain
         }, 
-        'user' : cpanel_user, 
-        'pass' : cpanel_pass
+        'user': cpanel_user,
+        'pass': cpanel_pass
     }
     
     return cpanel
 
-#TODO Add SSL certification
-def get_subdomain_property(cpanel, property):
+
+def get_subdomain_property(cpanel, setting):
     params = cpanel['params']
     params['cpanel_jsonapi_func'] = 'fetchzone'
     
-    response = requests.get(cpanel['url'], params, auth=(cpanel['user'], cpanel['pass']),  verify=False)
+    response = requests.get(cpanel['url'], params, auth=(cpanel['user'], cpanel['pass']),  verify=True)
     
     result = response.json()['cpanelresult']['data'][0]['record']
     
     if result:
-            return result[0][property]
+            return result[0][setting]
     
     return None
+
 
 def edit_subdomain_address(cpanel, dns_line, dns_address):
     params = cpanel['params']
@@ -40,20 +42,25 @@ def edit_subdomain_address(cpanel, dns_line, dns_address):
     params['Line'] = dns_line
     params['address'] = dns_address
     
-    requests.get(cpanel['url'], params, auth=(cpanel['user'], cpanel['pass']),  verify=False)
+    requests.get(cpanel['url'], params, auth=(cpanel['user'], cpanel['pass']),  verify=True)
 
-#Use external service to get IP address
+
 def get_ip_address():
-    response = requests.get('http://ipinfo.io/ip')
-    
-    return response.text
+    """
+    Use external service to get IP address
+    :return:
+    """
+    return requests.get('https://ipinfo.io/ip', verify=True).text
+
 
 def main():
     parser = argparse.ArgumentParser(description="Dynamic DNS updater")
     parser.add_argument("-u",  "--username",  help="cPanel username")
     parser.add_argument("-p",  "--password",  help="cPanel password")
     parser.add_argument("domain",  help="Domain to be modified")
-    parser.add_argument("sub_domain", nargs='+', help="List of sub-domain names to be updated (excluding the qualified domain suffix). eg. hello.example.com would simply translate to hello")
+    parser.add_argument("sub_domain", nargs='+', help="List of sub-domain names to be updated (excluding the "
+                                                      "qualified domain suffix). eg. hello.example.com would "
+                                                      "simply translate to hello")
     parser.add_argument("url", help="cPanel URL e.g. https://example.com:2083")
     
     args = parser.parse_args()
@@ -66,12 +73,12 @@ def main():
     
     ip_address = get_ip_address()
     
-    #Each subdomain shares the same address above
+    # Each subdomain shares the same address above
     for dns_sub in dns_subs:
         cpanel = create_cpanel(cpanel_user, cpanel_pass, cpanel_url, dns_domain, dns_sub)
         dns_address = get_subdomain_property(cpanel, 'address')
         
-        #Check if IP address has changed
+        # Check if IP address has changed
         if ip_address != dns_address:
             dns_line = get_subdomain_property(cpanel, 'line')
             edit_subdomain_address(cpanel, dns_line, ip_address)
